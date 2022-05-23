718e123a593c670db777759edfb9e25b042d8dc4
takeshi tsuchida <takeshi.tsuchida@gmail.com>
2022-05-23 20:20:55 +0900

    1st

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+interface AppProps {
+  message?: string;
+  description?: string;
+}
+
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>React Typescript</div>
+      <div>{ message }</div>
     </div>
-  );
+    )
+}
+
+App.defaultProps = {
+  message: 'Hello, defualtProps!',
+  description: 'This is App component'
 }
 
 export default App;
